2e288cc7455aaac5bddd943e54a463eea02375be
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2021-08-19 18:56:17 +0100

    Set up basic redirector

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$defaultUsername = "jahidulpabelislam";
+
+$links = [
+    "site" => "https://jahidulpabelislam.com",
+    "linkedin" => "https://www.linkedin.com/in/$defaultUsername/",
+    "github" => "https://github.com/$defaultUsername/",
+    "instagram" => "https://instagram.com/$defaultUsername/",
+    "npm" => "https://www.npmjs.com/~$defaultUsername",
+    "packagist" => "https://packagist.org/users/$defaultUsername/packages/",
+];
+
+$request = trim($_SERVER["REQUEST_URI"], "/");
+
+if (array_key_exists($request, $links)) {
+    header("Location: " . $links[$request]);
+    return;
+}
+
+header("HTTP/1.1 404 Not Found");
