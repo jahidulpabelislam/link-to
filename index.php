5f0cacdb4dfe55bac4f8b3f80110528e76cabc42
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2021-12-18 23:03:34 +0000

    Add link to links & socials pages

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ $defaultUsername = "jahidulpabelislam";
 
 $links = [
     "site" => "https://jahidulpabelislam.com",
+    "links" => "https://links.jahidulpabelislam.com",
+    "socials" => "https://socials.jahidulpabelislam.com",
     "linkedin" => "https://www.linkedin.com/in/$defaultUsername/",
     "github" => "https://github.com/$defaultUsername/",
     "instagram" => "https://instagram.com/$defaultUsername/",
