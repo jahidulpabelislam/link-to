d9d211acaff60d7a43032abe7b5499d18465bb0e
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2025-08-03 19:09:22 +0100

    Better support for multiple envs (#3)

--- public/error/404.php
+++ public/error/404.php
@@ -1,10 +1,10 @@
 <?php
 
-const PUBLIC_ROOT = __DIR__ . "/..";
+declare(strict_types=1);
 
 include_once(__DIR__ . "/../../vendor/autoload.php");
 
-$site = \JPI\Site::get();
+$app = \JPI\App::get();
 ?>
 
 <!DOCTYPE html>
@@ -20,16 +20,16 @@ $site = \JPI\Site::get();
 
         <meta name="robots" content="noindex" />
 
-        <?php $site->renderFavicons(); ?>
+        <?php $app->renderFavicons(); ?>
 
-        <link href="<?php echo $site::asset("/assets/css/error.css"); ?>" rel="stylesheet" type="text/css" media="all" title="style" />
+        <link href="<?php echo $app::asset("/assets/css/error.css"); ?>" rel="stylesheet" type="text/css" media="all" title="style" />
     </head>
 
     <body>
         <main class="page">
-            <p class="error">Hmm... Couldn't Find What You Are Looking For, But You Can Find All My Links <a href="https://links.jahidulpabelislam.com/">Here</a>.</p>
+            <p class="error">Hmm... Couldn't Find What You Are Looking For, But You Can Find All My Links <a href="https://links.<?php echo $app::DOMAINS[$app->getEnvironment()]; ?>">Here</a>.</p>
         </main>
 
-        <script src="<?php echo $site::asset("/assets/js/error.js"); ?>" type="application/javascript"></script>
+        <script src="<?php echo $app::asset("/assets/js/error.js"); ?>" type="application/javascript"></script>
     </body>
 </html>
